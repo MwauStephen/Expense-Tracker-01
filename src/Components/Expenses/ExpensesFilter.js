831801318e4d4f3f76a7ff09e702c64bfea24e04
Mwau stephen <stephenmwau41@gmail.com>
2022-05-20 06:02:10 +0800

    changed the event handler name

--- src/Components/Expenses/ExpensesFilter.js
+++ src/Components/Expenses/ExpensesFilter.js
@@ -3,14 +3,14 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = () => {
-  const filterChangeHandler = (event) => {
+  const dropDownChangeHandler = (event) => {
     console.log(event.target.value);
   };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={filterChangeHandler}>
+        <select onChange={dropDownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
