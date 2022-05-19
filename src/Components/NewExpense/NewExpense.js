a66cfafaa9fb141c86f571f49a9f07d5d49e429e
Mwau stephen <stephenmwau41@gmail.com>
2022-05-20 05:09:06 +0800

    accepted props and lifted state

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -2,13 +2,15 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    // lifiting state up
+    props.onAddExpense(expenseData);
+    // console.log(expenseData);
   };
   return (
     <div className="new-expense">
