a9471a6ef44984990b57572c58496f48267628ed
Mwau stephen <stephenmwau41@gmail.com>
2022-05-20 04:59:54 +0800

    added a prop

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -26,7 +26,9 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    // console.log(expenseData);
+
+    props.onSaveExpenseData(expenseData);
 
     // clearing the user inputs using two-way binding
     setEnteredTitle("");
