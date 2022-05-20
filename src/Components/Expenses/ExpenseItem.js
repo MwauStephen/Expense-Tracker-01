ca18458c572cc3e598ce13184eb201b58e0033dc
Mwau stephen <stephenmwau41@gmail.com>
2022-05-20 21:52:27 +0800

    passed a prop to the title

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,6 +5,7 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
     setTitle("updated !🤓🚴‍♂️🚴‍♂️");
     console.log(title);
@@ -13,7 +14,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
