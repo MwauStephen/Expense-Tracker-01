d0df7fb135a820fdf2e04b0691994eb5b6009dc9
Mwau stephen <stephenmwau41@gmail.com>
2022-07-04 18:59:45 +0800

    finished the expense tracker

--- src/Components/Expenses/ExpenseChart.js
+++ src/Components/Expenses/ExpenseChart.js
@@ -3,22 +3,22 @@ import Chart from "../Chart/Chart";
 
 const ExpenseChart = (props) => {
   const chartDataPoints = [
-    { label: "Jan", Value: 0 },
-    { label: "Feb", Value: 0 },
-    { label: "Mar", Value: 0 },
-    { label: "Apr", Value: 0 },
-    { label: "May", Value: 0 },
-    { label: "Jun", Value: 0 },
-    { label: "July", Value: 0 },
-    { label: "Aug", Value: 0 },
-    { label: "Sept", Value: 0 },
-    { label: "Oct", Value: 0 },
-    { label: "Nov", Value: 0 },
-    { label: "Dec", Value: 0 },
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "July", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sept", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
   ];
   for (const expense in props.expense) {
     const expenseMonth = expense.date.getMonth();
-    chartDataPoints[expenseMonth].Value += expense.amount;
+    chartDataPoints[expenseMonth].value += expense.amount;
   }
   return <Chart dataPoints={chartDataPoints} />;
 };
